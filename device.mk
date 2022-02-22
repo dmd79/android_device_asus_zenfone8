980ed6aae150bfe81e1518ecd1c1bf1cd820f958
dmd79 <davidevinavil@gmail.com>
2022-02-22 20:45:15 +0100

    zenfone8: Drop UDFPS

--- device.mk
+++ device.mk
@@ -106,11 +106,6 @@ PRODUCT_PACKAGES += \
 #PRODUCT_PACKAGES += libbluetooth_qti
 #PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_dun-V1.0-java
 
-# Biometric
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.asus_lahaina \
-    UdfpsResources
-
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
